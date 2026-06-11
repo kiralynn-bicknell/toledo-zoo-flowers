72aba9d8d169f1e0a7af018856dc91bd0a8de5a1
kiralynn-bicknell <kiralynn.bicknell@gmail.com>
2026-06-11 18:09:36 -0400

    tweaking

--- layers/layers.js
+++ layers/layers.js
@@ -29,6 +29,7 @@ var lyr_ToledoZooFlowerSurvey_1 = new ol.layer.Vector({
                 declutter: false,
                 source:cluster_ToledoZooFlowerSurvey_1, 
                 style: style_ToledoZooFlowerSurvey_1,
+ 		popuplayertitle: 'Toledo Zoo Flower Survey',
                 interactive: true,
     title: 'Toledo Zoo Flower Survey 2026<br />\
     <img src="styles/legend/ToledoZooFlowerSurvey_1_0.png" /> Native to Ohio<br />\
