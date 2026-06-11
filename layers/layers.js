0fd3cc0a9e5faa95d7b2f20d13b2e75108671555
kiralynn-bicknell <kiralynn.bicknell@gmail.com>
2026-06-11 18:30:45 -0400

    Add popuplayertitle to Toledo Zoo layer

--- layers/layers.js
+++ layers/layers.js
@@ -29,6 +29,7 @@ var lyr_ToledoZooFlowerSurvey_1 = new ol.layer.Vector({
                 declutter: false,
                 source:cluster_ToledoZooFlowerSurvey_1, 
                 style: style_ToledoZooFlowerSurvey_1,
+                popuplayertitle: ' ',
                 interactive: true,
     title: 'Toledo Zoo Flower Survey 2026<br />\
     <img src="styles/legend/ToledoZooFlowerSurvey_1_0.png" /> Native to Ohio<br />\
